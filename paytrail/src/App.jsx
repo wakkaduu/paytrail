f1e806c954a04b66d3f23e2ff791928fbc5cdf21
wakkaduu <savarizzlawrence@gmail.com>
2026-05-28 04:27:48 +0800

    Initial commit

---
+++ paytrail/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-zinc-950 text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        PayTrail
+      </h1>
+    </div>
+  )
+}
